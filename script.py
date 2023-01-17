bf3819a196677d9f52cb9de372539dc6aa958320
mirzomumin <mirzomumin@list.ru>
2023-01-17 12:50:13 +0500

    Added model fixtures

--- script.py
+++ script.py
@@ -1,29 +1,25 @@
+import requests
+
 from book.models import Author, Category, Book
-import json
 
-with open('books.json') as f:
-	data = json.load(f)
-
-books = data['response']['books']
-price = 15.99
-
-for book in books:
-	book_name = book['title']
-	description = book['description']
-	if description == '':
-		description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-	category = book['categories']
-	if category == '':
-		category = 'Computer Science'
-	my_category, created = Category.objects.get_or_create(name=category)
-	print(my_category, type(my_category))
-	my_book, created = Book.objects.get_or_create(name=book_name, description=description, price=price)
-	print(my_book, type(my_book))
-	my_book.category.add(my_category)
-	my_book.save()
-	authors = book['author'].split(', ')
-	for author in authors:
-		my_author, created = Author.objects.get_or_create(name=author)
-		my_book.author.add(my_author)
-		my_book.save()
-	price += 1
+def create_book(index):
+	response = requests.get(f'https://www.googleapis.com/books/v1/volumes?q=books&filter=ebooks&printType=books&maxResults=40&startIndex={index}')
+	items = response.json()['items']
+	new_items = []
+	for item in items:
+		if ('authors' in item['volumeInfo'].keys() and 'categories' in item['volumeInfo'].keys()
+			and 'description' in item['volumeInfo'].keys() and 'listPrice' in item['saleInfo'].keys()):
+			book_name = item['volumeInfo']['title']
+			description = item['volumeInfo']['description']
+			price = item['saleInfo']['listPrice']['amount']
+			categories = item['volumeInfo']['categories']
+			authors = item['volumeInfo']['authors']
+			book, created = Book.objects.get_or_create(name=book_name, description=description, price=price)
+			for category in categories:
+				book_category, created = Category.objects.get_or_create(name=category)
+				book.category.add(book_category)
+				book.save()
+			for author in authors:
+				book_author, created = Author.objects.get_or_create(name=author)
+				book.author.add(book_author)
+				book.save()
